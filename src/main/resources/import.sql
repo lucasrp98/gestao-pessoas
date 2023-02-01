7110e4c2f436dcc656c95fe3caeeddfd01ce7e81
Lucas Ribeiro <lucaorpacheco@gmail.com>
2023-01-31 21:59:25 -0300

    Ajuste do Banco

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,6 @@
 INSERT INTO tb_user(name, data_nasc) VALUES ('Lucas', '11/02/1998');
 INSERT INTO tb_user(name, data_nasc) VALUES ('Laura', '12/01/1996');
 
-INSERT INTO tb_endereco(rua, user_id) VALUES ('Rua A', '1');
-INSERT INTO tb_endereco(rua, user_id) VALUES ('Rua B', '1');
-INSERT INTO tb_endereco(rua, user_id) VALUES ('Rua C', '2');
+INSERT INTO tb_endereco(rua, user_id, cep, cidade, num_casa) VALUES ('Rua A', '1', '96180-000', 'Camaquã', '1');
+INSERT INTO tb_endereco(rua, user_id, cep, cidade, num_casa) VALUES ('Rua B', '1', '96180-720', 'Camaquã', '12');
+INSERT INTO tb_endereco(rua, user_id, cep, cidade, num_casa) VALUES ('Rua C', '2', '94180-000', 'Canoas', '13');
